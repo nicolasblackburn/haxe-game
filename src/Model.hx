d46dd3e110bdf9dbf404659f88f3252f46841622
Nicolas Blackburn <nicolas@nblackburn.ca>
2021-04-29 19:27:06 -0400

    Fixed keyboard controls, integer collision response, deactivated corner slides

--- src/Model.hx
+++ src/Model.hx
@@ -100,30 +100,47 @@ class Model implements PhysicsModel {
 
     var bottomCorner = topCorner.clone().add(new Point2D(0, body.bounds.height));
     
-    var correctionX = if (displacement.x >= 0) {
-      -(body.position.x + body.bounds.x + body.bounds.width) % this.world.tileSize.x;
+    if (displacement.x >= 0) {
+      body.position.x = Math.floor((body.position.x + body.bounds.x + body.bounds.width) / this.world.tileSize.x) * this.world.tileSize.x - body.bounds.x - body.bounds.width;
     } else {
-      this.world.tileSize.x - (body.position.x + body.bounds.x) % this.world.tileSize.x;
+      body.position.x = Math.ceil((body.position.x + body.bounds.x) / this.world.tileSize.x) * this.world.tileSize.x;
     }
-    body.position.x += correctionX;
-
+    
+    /*
     if (!this.world.canMovePoint(topCorner) && this.world.canMovePoint(bottomCorner)) {
       var nudgeY = Math.min(
-        Math.abs(body.velocity.x) / Math.sqrt(2),
+        Math.abs(displacement.x) / Math.sqrt(2),
         this.world.tileSize.y - topCorner.y % this.world.tileSize.y
+      );
+      trace(
+        "resolveX nudge 1",
+        body.position.x,
+        body.position.y,
+        Math.abs(displacement.x) / Math.sqrt(2),
+        this.world.tileSize.y - topCorner.y % this.world.tileSize.y,
+        nudgeY
       );
       if (nudgeY > displacement.y) {
         body.velocity.y = nudgeY;
       }
     } else if (this.world.canMovePoint(topCorner) && !this.world.canMovePoint(bottomCorner)) {
       var nudgeY = Math.max(
-        -Math.abs(body.velocity.x) / Math.sqrt(2),
+        -Math.abs(displacement.x) / Math.sqrt(2),
         -bottomCorner.y % this.world.tileSize.y
+      );
+      trace(
+        "resolveX nudge 2",
+        body.position.x,
+        body.position.y,
+        Math.abs(displacement.x) / Math.sqrt(2),
+        this.world.tileSize.y - topCorner.y % this.world.tileSize.y,
+        nudgeY
       );
       if (nudgeY < displacement.y) {
         body.velocity.y = nudgeY;
       }
     }
+      */
   }
 
   private function resolveCollisionY(body: Body, displacement: Point2D) {
@@ -135,16 +152,16 @@ class Model implements PhysicsModel {
 
     var rightCorner = leftCorner.clone().add(new Point2D(body.bounds.width, 0));
 
-    var correctionY = if (displacement.y >= 0) {
-      -(body.position.y + body.bounds.y + body.bounds.height) % this.world.tileSize.y;
+     if (displacement.y >= 0) {
+      body.position.y = Math.floor((body.position.y + body.bounds.y + body.bounds.height) / this.world.tileSize.y) * this.world.tileSize.y - body.bounds.y - body.bounds.height;
     } else {
-      this.world.tileSize.y - (body.position.y + body.bounds.y) % this.world.tileSize.y;
+      body.position.y = Math.ceil((body.position.y + body.bounds.y) / this.world.tileSize.y) * this.world.tileSize.y;
     }
-    body.position.y += correctionY;
 
+    /*
     if (!this.world.canMovePoint(leftCorner) && this.world.canMovePoint(rightCorner)) {
       var nudgeX = Math.min(
-        Math.abs(body.velocity.y) / Math.sqrt(2),
+        Math.abs(displacement.y) / Math.sqrt(2),
         this.world.tileSize.x - leftCorner.x % this.world.tileSize.x
       );
       if (nudgeX > displacement.x) {
@@ -152,12 +169,13 @@ class Model implements PhysicsModel {
       }
     } else if (this.world.canMovePoint(leftCorner) && !this.world.canMovePoint(rightCorner)) {
       var nudgeX = Math.max(
-        -Math.abs(body.velocity.y) / Math.sqrt(2),
-        -rightCorner.x % this.world.tileSize.x
+        -Math.abs(displacement.y) / Math.sqrt(2),
+        -rightCorner.x % (2 * this.world.tileSize.x)
       );
       if (nudgeX < displacement.x) {
         body.position.x += nudgeX;
       }
     }
+    */
   }
 }
--- src/gamepad/keyboard/KeyboardGamepad.hx
+++ src/gamepad/keyboard/KeyboardGamepad.hx
@@ -1,5 +1,6 @@
 package gamepad.keyboard;
 
+import geom.Point2D;
 import gamepad.events.ButtonReleasedEvent;
 import gamepad.events.AxisReleasedEvent;
 import gamepad.events.ButtonPressedEvent;
@@ -12,9 +13,14 @@ private typedef Keymap = {
   var type: String;
   var index: Int;
   var value: Float;
+  var ?reverseKey: String;
 }
 
 class KeyboardGamepad extends Emitter implements Gamepad {
+  public var axes = [0.0, 0.0];
+  public var buttons = [{value: 0.0, pressed: false}];
+
+  private var keysStack: Array<String> = [];
   private var mappings: Map<String, Keymap> = [
     "ArrowUp" => {type: "axis", index: 1, value: -1.0},
     "ArrowRight" => {type: "axis", index: 0, value: 1.0},
@@ -22,8 +28,7 @@ class KeyboardGamepad extends Emitter implements Gamepad {
     "ArrowLeft" => {type: "axis", index: 0, value: -1.0},
     "a" => {type: "button", index: 0, value: 1.0}
   ];
-  public var axes = [0.0, 0.0];
-  public var buttons = [{value: 0.0, pressed: false}];
+  private var pureAxes = [0.0, 0.0];
 
   public function new() {
     super();
@@ -37,12 +42,17 @@ class KeyboardGamepad extends Emitter implements Gamepad {
       var keymap = this.mappings[event.key];
       switch (keymap.type) {
         case "axis":
-          if (this.axes[keymap.index] != keymap.value) {
-            this.axes[keymap.index] = keymap.value;
+          if (this.pureAxes[keymap.index] != keymap.value) {
+            this.keysStack.push(event.key);
+            this.pureAxes[keymap.index] = keymap.value;
+            var normalized = new Point2D(this.pureAxes[0], this.pureAxes[1]).normalize();
+            this.axes[0] = normalized.x;
+            this.axes[1] = normalized.y;
             this.emit('axispressed', new AxisPressedEvent(this.axes[0], this.axes[1], [0, 1]));
           }
         case "button":
           if (this.buttons[keymap.index].value != keymap.value) {
+            this.keysStack.push(event.key);
             this.buttons[keymap.index].value = keymap.value;
             this.buttons[keymap.index].pressed = true;
             this.emit('buttonpressed', new ButtonPressedEvent(keymap.value, keymap.index));
@@ -56,17 +66,35 @@ class KeyboardGamepad extends Emitter implements Gamepad {
       var keymap = this.mappings[event.key];
       switch (keymap.type) {
         case "axis":
-          if (this.axes[keymap.index] != 0.0) {
-            this.axes[keymap.index] = 0.0;
+          this.keysStack = this.keysStack.filter(key -> key != event.key);
+          if (reverseKeyInStack(event.key)) {
+            this.pureAxes[keymap.index] = -this.pureAxes[keymap.index];
+          } else {
+            this.pureAxes[keymap.index] = 0.0;
+          }
+          var normalized = new Point2D(this.pureAxes[0], this.pureAxes[1]).normalize();
+          this.axes[0] = normalized.x;
+          this.axes[1] = normalized.y;
+          if (this.axes[0] == 0.0 && this.axes[1] == 0.0) {
             this.emit('axisreleased', new AxisReleasedEvent([0, 1]));
+          } else {
+            this.emit('axispressed', new AxisPressedEvent(this.axes[0], this.axes[1], [0, 1]));
           }
         case "button":
-          if (this.buttons[keymap.index].value != 0.0) {
-            this.buttons[keymap.index].value = 0.0;
-            this.buttons[keymap.index].pressed = false;
-            this.emit('buttonreleased', new ButtonReleasedEvent(keymap.index));
-          }
+          this.keysStack = this.keysStack.filter(key -> key != event.key);
+          this.buttons[keymap.index].value = 0.0;
+          this.buttons[keymap.index].pressed = false;
+          this.emit('buttonreleased', new ButtonReleasedEvent(keymap.index));
       }
     }
+  }
+
+  private function reverseKeyInStack(key: String) {
+    for (reverseKey => map in this.mappings) {
+      if (map.index == this.mappings[key].index && map.value == -this.mappings[key].value && this.keysStack.indexOf(reverseKey) >= 0) {
+        return true;
+      }
+    }
+    return false;
   }
 }
